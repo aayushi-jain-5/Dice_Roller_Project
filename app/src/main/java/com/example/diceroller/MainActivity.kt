5f8ad2b0d13838a6c27e0cb4aeafce5e18ef425d
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2023-02-22 17:45:30 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollbutton: Button = findViewById(R.id.button)
+        val textview: TextView = findViewById(R.id.textView)
+        val imageview: ImageView = findViewById(R.id.imageView)
+
+        rollbutton.setOnClickListener{
+          Toast.makeText(this,"Dice Rolled",Toast.LENGTH_SHORT).show()
+
+            var c: Int = (1..6).random()
+
+            when(c){
+                1->imageview.setImageResource(R.drawable.dice_1)
+                2->imageview.setImageResource(R.drawable.dice_2)
+                3->imageview.setImageResource(R.drawable.dice_3)
+                4->imageview.setImageResource(R.drawable.dice_4)
+                5->imageview.setImageResource(R.drawable.dice_5)
+                6->imageview.setImageResource(R.drawable.dice_6)
+                else->imageview.setImageResource(R.drawable.dice_6)
+            }
+
+
+            textview.text= c.toString()
+        }
+    }
+}
